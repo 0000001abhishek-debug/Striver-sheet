88e06f3d765ca08801bd27603bc711633ba619f0
Abhishek Kumar <yourgithubemail@gmail.com>
2026-02-17 23:57:21 +0530

    Easy Array

--- Easy Array/Main.java
+++ Easy Array/Main.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int secondLargestbrute(int[] arr) 
+    public int secondLargest(int[] arr) 
     {
         int n = arr.length;
         int largest = arr[0];
@@ -22,5 +22,15 @@ class Solution {
             return -1;
         }
         return secondLargest;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 8, 2, 10, 3};
+
+        System.out.println("Second Largest: " + Solution.secondLargest(arr));
     }
 }
