86513aaab4491859ca402db8f545cfbf4ec783e7
Abhishek Kumar <yourgithubemail@gmail.com>
2026-04-28 18:14:50 +0530

    JAVA

--- Java Core/Pratice/Pratice3.java
+++ Java Core/Pratice/Pratice3.java
@@ -65,31 +65,23 @@
 //     int[] position = {1, 2, 3};
 //     Pratice3 obj = new Pratice3();
 //     int result = obj.minCostToMoveChips(position);
-//     System.out.println("Minimum cost to move chips:" + result);
-//   }
-// }
+//     System.out.println("Minimum cost to move chips:" + result);}}
 
 //4. Remove Duplicates from Sorted Array
-class Pratice3 {
-  public int removeDuplicate(int[] nums) {
-    if (nums.length == 0) {
-      return 0;
-    }
-    int uniqueCount = 1;
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i] != nums[i - 1]) {
-        nums[uniqueCount] = nums[i];
-        uniqueCount++;
-      }
-      
-    }
-    return uniqueCount;
-  }
-
-  public static void main(String[] args) {
-    int[] nums = { 1, 1, 2, 2, 3, 4, 4, 5 };
-    Pratice3 obj = new Pratice3();
-    int newLength = obj.removeDuplicate(nums);
-    System.out.println("New length after removing duplicates: " + newLength);
-  }
-}
+// class Pratice3 {
+//   public int removeDuplicate(int[] nums) {
+//     if (nums.length == 0) {
+//       return 0;
+//     }
+//     int uniqueCount = 1;
+//     for (int i = 1; i < nums.length; i++) {
+//       if (nums[i] != nums[i - 1]) {
+//         nums[uniqueCount] = nums[i];
+//         uniqueCount++;
+//       }}
+//     return uniqueCount; }
+//   public static void main(String[] args) {
+//     int[] nums = { 1, 1, 2, 2, 3, 4, 4, 5 };
+//     Pratice3 obj = new Pratice3();
+//     int newLength = obj.removeDuplicate(nums);
+//     System.out.println("New length after removing duplicates: " + newLength);}}
